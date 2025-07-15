4cea3e8d0a054021d0e57271106803a13449ae1d
Andrey Nekrasov <bonerdelli@gmail.com>
2025-07-15 14:38:38 +0200

    PLA-1244 Fix build and linter issues

--- src/hooks/useTransactionDeadline.ts
+++ src/hooks/useTransactionDeadline.ts
@@ -1,5 +1,4 @@
 import { BigNumber } from '@ethersproject/bignumber'
-import { useWeb3React } from '@web3-react/core'
 import { useMemo } from 'react'
 import { useAppSelector } from 'state/hooks'
 
@@ -7,11 +6,10 @@ import useCurrentBlockTimestamp from './useCurrentBlockTimestamp'
 
 // combines the block timestamp with the user setting to give the deadline that should be used for any submitted transaction
 export default function useTransactionDeadline(): BigNumber | undefined {
-  const { chainId } = useWeb3React()
   const ttl = useAppSelector((state) => state.user.userDeadline)
   const blockTimestamp = useCurrentBlockTimestamp()
   return useMemo(() => {
     if (blockTimestamp && ttl) return blockTimestamp.add(ttl)
     return undefined
-  }, [blockTimestamp, chainId, ttl])
+  }, [blockTimestamp, ttl])
 }
