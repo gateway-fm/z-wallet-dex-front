c3b84e9720f6113ca7687824a8678efe71015141
Andrey Nekrasov <bonerdelli@gmail.com>
2025-07-16 09:45:42 +0200

    PLA-1244 Fix craco configuration

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,25 +1,31 @@
 /* eslint-env node */
-const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin')
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
-const { execSync } = require('child_process')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin')
-const { IgnorePlugin, ProvidePlugin } = require('webpack')
-const { RetryChunkLoadPlugin } = require('webpack-retry-chunk-load-plugin')
+const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const { execSync } = require("child_process");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
+const { IgnorePlugin, ProvidePlugin } = require("webpack");
+const { RetryChunkLoadPlugin } = require("webpack-retry-chunk-load-plugin");
 
-const commitHash = execSync('git rev-parse HEAD').toString().trim()
-const isProduction = process.env.NODE_ENV === 'production'
+let commitHash = "unknown";
+try {
+  commitHash = execSync("git rev-parse HEAD").toString().trim();
+} catch (error) {
+  // Git not available in CI/CD environment
+  console.log("Git not available, using default commit hash");
+}
+const isProduction = process.env.NODE_ENV === "production";
 
-process.env.REACT_APP_GIT_COMMIT_HASH = commitHash
+process.env.REACT_APP_GIT_COMMIT_HASH = commitHash;
 
 // Linting and type checking are only necessary as part of development and testing.
 // Omit them from production builds, as they slow down the feedback loop.
-const shouldLintOrTypeCheck = !isProduction
+const shouldLintOrTypeCheck = !isProduction;
 
 function getCacheDirectory(cacheName) {
   // Include the trailing slash to denote that this is a directory.
-  return `${path.join(__dirname, 'node_modules/.cache/', cacheName)}/`
+  return `${path.join(__dirname, "node_modules/.cache/", cacheName)}/`;
 }
 
 module.exports = {
@@ -28,14 +34,14 @@ module.exports = {
     pluginOptions(eslintConfig) {
       return Object.assign(eslintConfig, {
         cache: true,
-        cacheLocation: getCacheDirectory('eslint'),
-        ignorePath: '.gitignore',
+        cacheLocation: getCacheDirectory("eslint"),
+        ignorePath: ".gitignore",
         // Use our own eslint/plugins/config, as overrides interfere with caching.
         // This ensures that `yarn start` and `yarn lint` share one cache.
-        eslintPath: require.resolve('eslint'),
+        eslintPath: require.resolve("eslint"),
         resolvePluginsRelativeTo: null,
         baseConfig: null,
-      })
+      });
     },
   },
   typescript: {
@@ -44,23 +50,26 @@ module.exports = {
   jest: {
     configure(jestConfig) {
       return Object.assign(jestConfig, {
-        cacheDirectory: getCacheDirectory('jest'),
+        cacheDirectory: getCacheDirectory("jest"),
         transform: {
-          ...Object.entries(jestConfig.transform).reduce((transform, [key, value]) => {
-            if (value.match(/babel/)) return transform
-            return { ...transform, [key]: value }
-          }, {}),
+          ...Object.entries(jestConfig.transform).reduce(
+            (transform, [key, value]) => {
+              if (value.match(/babel/)) return transform;
+              return { ...transform, [key]: value };
+            },
+            {}
+          ),
           // Transform vanilla-extract using its own transformer.
           // See https://sandroroth.com/blog/vanilla-extract-cra#jest-transform.
-          '\\.css\\.ts$': '@vanilla-extract/jest-transform',
-          '\\.(t|j)sx?$': '@swc/jest',
+          "\\.css\\.ts$": "@vanilla-extract/jest-transform",
+          "\\.(t|j)sx?$": "@swc/jest",
         },
         // Use d3-arrays's build directly, as jest does not support its exports.
-        transformIgnorePatterns: ['d3-array'],
+        transformIgnorePatterns: ["d3-array"],
         moduleNameMapper: {
-          'd3-array': 'd3-array/dist/d3-array.min.js',
+          "d3-array": "d3-array/dist/d3-array.min.js",
         },
-      })
+      });
     },
   },
   webpack: {
@@ -68,7 +77,7 @@ module.exports = {
       // Webpack 5 does not polyfill node globals, so we do so for those necessary:
       new ProvidePlugin({
         // - react-markdown requires process.cwd
-        process: 'process/browser.js',
+        process: "process/browser.js",
       }),
       new VanillaExtractPlugin(),
       new RetryChunkLoadPlugin({
@@ -89,27 +98,27 @@ module.exports = {
           // CSS ordering is mitigated through scoping / naming conventions, so we can ignore order warnings.
           // See https://webpack.js.org/plugins/mini-css-extract-plugin/#remove-order-warnings.
           if (plugin instanceof MiniCssExtractPlugin) {
-            plugin.options.ignoreOrder = true
+            plugin.options.ignoreOrder = true;
           }
 
           // Disable TypeScript's config overwrite, as it interferes with incremental build caching.
           // This ensures that `yarn start` and `yarn typecheck` share one cache.
-          if (plugin.constructor.name == 'ForkTsCheckerWebpackPlugin') {
-            delete plugin.options.typescript.configOverwrite
+          if (plugin.constructor.name == "ForkTsCheckerWebpackPlugin") {
+            delete plugin.options.typescript.configOverwrite;
           }
 
-          return plugin
+          return plugin;
         })
         .filter((plugin) => {
           // Case sensitive paths are already enforced by TypeScript.
           // See https://www.typescriptlang.org/tsconfig#forceConsistentCasingInFileNames.
-          if (plugin instanceof CaseSensitivePathsPlugin) return false
+          if (plugin instanceof CaseSensitivePathsPlugin) return false;
 
           // IgnorePlugin is used to tree-shake moment locales, but we do not use moment in this project.
-          if (plugin instanceof IgnorePlugin) return false
+          if (plugin instanceof IgnorePlugin) return false;
 
-          return true
-        })
+          return true;
+        });
 
       // Configure webpack resolution:
       webpackConfig.resolve = Object.assign(webpackConfig.resolve, {
@@ -118,43 +127,49 @@ module.exports = {
           // This is necessary because create-react-app guards against external imports.
           // See https://sandroroth.com/blog/vanilla-extract-cra#production-build.
           if (plugin instanceof ModuleScopePlugin) {
-            plugin.allowedPaths.push(path.join(__dirname, 'node_modules/@vanilla-extract/webpack-plugin'))
+            plugin.allowedPaths.push(
+              path.join(
+                __dirname,
+                "node_modules/@vanilla-extract/webpack-plugin"
+              )
+            );
           }
 
-          return plugin
+          return plugin;
         }),
         // Webpack 5 does not resolve node modules, so we do so for those necessary:
         fallback: {
           // - react-markdown requires path
-          path: require.resolve('path-browserify'),
+          path: require.resolve("path-browserify"),
         },
-      })
+      });
 
       // Retain source maps for node_modules packages:
       webpackConfig.module.rules[0] = {
         ...webpackConfig.module.rules[0],
         exclude: /node_modules/,
-      }
+      };
 
       // Configure webpack transpilation (create-react-app specifies transpilation rules in a oneOf):
-      webpackConfig.module.rules[1].oneOf = webpackConfig.module.rules[1].oneOf.map((rule) => {
-        if (rule.loader && rule.loader.match(/babel-loader/)) {
-          rule.loader = 'swc-loader'
-          delete rule.options
-        }
-        return rule
-      })
+      webpackConfig.module.rules[1].oneOf =
+        webpackConfig.module.rules[1].oneOf.map((rule) => {
+          if (rule.loader && rule.loader.match(/babel-loader/)) {
+            rule.loader = "swc-loader";
+            delete rule.options;
+          }
+          return rule;
+        });
 
       // Run terser compression on node_modules before tree-shaking, so that tree-shaking is more effective.
       // This works by eliminating dead code, so that webpack can identify unused imports and tree-shake them;
       // it is only necessary for node_modules - it is done through linting for our own source code -
       // see https://medium.com/engineering-housing/dead-code-elimination-and-tree-shaking-at-housing-part-1-307a94b30f23#7e03:
       webpackConfig.module.rules.push({
-        enforce: 'post',
+        enforce: "post",
         test: /node_modules.*\.(js)$/,
-        loader: path.join(__dirname, 'scripts/terser-loader.js'),
+        loader: path.join(__dirname, "scripts/terser-loader.js"),
         options: { compress: true, mangle: false },
-      })
+      });
 
       // Configure webpack optimization:
       webpackConfig.optimization = Object.assign(
@@ -168,16 +183,18 @@ module.exports = {
                 // See https://github.com/facebook/create-react-app/blob/d960b9e/packages/react-scripts/config/webpack.config.js#L713-L716.
                 maxSize: 5 * 1024 * 1024,
                 // Optimize over all chunks, instead of async chunks (the default), so that initial chunks are also optimized.
-                chunks: 'all',
+                chunks: "all",
               },
             }
           : {}
-      )
+      );
 
       // Configure webpack resolution. webpackConfig.cache is unused with swc-loader, but the resolver can still cache:
-      webpackConfig.resolve = Object.assign(webpackConfig.resolve, { unsafeCache: true })
+      webpackConfig.resolve = Object.assign(webpackConfig.resolve, {
+        unsafeCache: true,
+      });
 
-      return webpackConfig
+      return webpackConfig;
     },
   },
-}
+};
