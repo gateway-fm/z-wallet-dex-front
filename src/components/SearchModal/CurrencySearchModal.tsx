58f22c2b1fb44a7411284405af718fe0c65c1179
Andrey Nekrasov <bonerdelli@gmail.com>
2025-07-16 13:04:12 +0200

    PLA-1244 Remove tokens safety checking

--- src/components/SearchModal/CurrencySearchModal.tsx
+++ src/components/SearchModal/CurrencySearchModal.tsx
@@ -1,9 +1,6 @@
-import { Currency, Token } from '@uniswap/sdk-core'
-import TokenSafety from 'components/TokenSafety'
-import { memo, useCallback, useEffect, useState } from 'react'
-import { useUserAddedTokens } from 'state/user/hooks'
+import { Currency } from '@uniswap/sdk-core'
+import { memo, useCallback } from 'react'
 
-import useLast from '../../hooks/useLast'
 import { useWindowSize } from '../../hooks/useWindowSize'
 import Modal from '../Modal'
 import { CurrencySearch } from './CurrencySearch'
@@ -20,12 +17,6 @@ interface CurrencySearchModalProps {
   onlyShowCurrenciesWithBalance?: boolean
 }
 
-enum CurrencyModalView {
-  search,
-  importToken,
-  tokenSafety,
-}
-
 export default memo(function CurrencySearchModal({
   isOpen,
   onDismiss,
@@ -37,73 +28,38 @@ export default memo(function CurrencySearchModal({
   disableNonToken = false,
   onlyShowCurrenciesWithBalance = false,
 }: CurrencySearchModalProps) {
-  const [modalView, setModalView] = useState<CurrencyModalView>(CurrencyModalView.search)
-  const lastOpen = useLast(isOpen)
-  const userAddedTokens = useUserAddedTokens()
-
-  useEffect(() => {
-    if (isOpen && !lastOpen) {
-      setModalView(CurrencyModalView.search)
-    }
-  }, [isOpen, lastOpen])
-
-  const showTokenSafetySpeedbump = (token: Token) => {
-    setWarningToken(token)
-    setModalView(CurrencyModalView.tokenSafety)
-  }
-
   const handleCurrencySelect = useCallback(
-    (currency: Currency, hasWarning?: boolean) => {
-      if (hasWarning && currency.isToken && !userAddedTokens.find((token) => token.equals(currency))) {
-        showTokenSafetySpeedbump(currency)
-      } else {
-        onCurrencySelect(currency)
-        onDismiss()
-      }
+    (currency: Currency, _hasWarning?: boolean) => {
+      // Always select directly without safety modal
+      onCurrencySelect(currency)
+      onDismiss()
     },
-    [onDismiss, onCurrencySelect, userAddedTokens]
+    [onDismiss, onCurrencySelect]
   )
-  // used for token safety
-  const [warningToken, setWarningToken] = useState<Token | undefined>()
 
   const { height: windowHeight } = useWindowSize()
   // change min height if not searching
   let modalHeight: number | undefined = 80
   let content = null
-  switch (modalView) {
-    case CurrencyModalView.search:
-      if (windowHeight) {
-        // Converts pixel units to vh for Modal component
-        modalHeight = Math.min(Math.round((680 / windowHeight) * 100), 80)
-      }
-      content = (
-        <CurrencySearch
-          isOpen={isOpen}
-          onDismiss={onDismiss}
-          onCurrencySelect={handleCurrencySelect}
-          selectedCurrency={selectedCurrency}
-          otherSelectedCurrency={otherSelectedCurrency}
-          showCommonBases={showCommonBases}
-          showCurrencyAmount={showCurrencyAmount}
-          disableNonToken={disableNonToken}
-          onlyShowCurrenciesWithBalance={onlyShowCurrenciesWithBalance}
-        />
-      )
-      break
-    case CurrencyModalView.tokenSafety:
-      modalHeight = undefined
-      if (warningToken) {
-        content = (
-          <TokenSafety
-            tokenAddress={warningToken.address}
-            onContinue={() => handleCurrencySelect(warningToken)}
-            onCancel={() => setModalView(CurrencyModalView.search)}
-            showCancel={true}
-          />
-        )
-      }
-      break
+
+  if (windowHeight) {
+    // Converts pixel units to vh for Modal component
+    modalHeight = Math.min(Math.round((680 / windowHeight) * 100), 80)
   }
+
+  content = (
+    <CurrencySearch
+      isOpen={isOpen}
+      onDismiss={onDismiss}
+      onCurrencySelect={handleCurrencySelect}
+      selectedCurrency={selectedCurrency}
+      otherSelectedCurrency={otherSelectedCurrency}
+      showCommonBases={showCommonBases}
+      showCurrencyAmount={showCurrencyAmount}
+      disableNonToken={disableNonToken}
+      onlyShowCurrenciesWithBalance={onlyShowCurrenciesWithBalance}
+    />
+  )
   return (
     <Modal isOpen={isOpen} onDismiss={onDismiss} height={modalHeight}>
       {content}
